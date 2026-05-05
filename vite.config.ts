352bcf8884b15b2edcfe9188ba9c0e8afad2a526
HumayunNaseer <humayun.naseer@tkxel.io>
2026-05-05 15:55:16 +0500

    initial portfolio setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/YOUR-REPO-NAME/",   // 👈 IMPORTANT FIX
+  base: "Portfolio",   // 👈 IMPORTANT FIX
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
